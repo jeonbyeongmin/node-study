7c22bbc88a3cdc3d944be0930006c233f1998f2b
jeonbyeongmin <qudals7613@gmail.com>
2022-03-13 03:24:40 +0900

    feat: node init mongodb mongoose

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const port = 4000;
+
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://jeonbyeongmin:qudals7613@youtube-clone.bmivc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
